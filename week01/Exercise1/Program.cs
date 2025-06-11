4e2f375fb25adaa15c0e11502048ddebb7cb9f2e
Dave Zeller <59622979+davewzeller@users.noreply.github.com>
2025-06-11 12:03:54 -0700

    Trying up commit on big computer.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+         
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        //  Console.WriteLine(firstName);
+        //  Console.WriteLine(lastName);
+
+        Console.WriteLine();
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
