25279ba7dfb56d28eb18383f17a42eb5cca90aff
Dave Zeller <59622979+davewzeller@users.noreply.github.com>
2025-06-11 22:49:14 -0700

    Loops are going to kill me.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.Write("What is your magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int magic = int.Parse(magicNumber);
+        // I have a hard time with loops!
+
+
+        //Console.WriteLine();
+        // Console.Write("What is your Guess? ");
+
+        // string Number = Console.ReadLine();
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+
+        int number = 65;
+
+        while (number != magic)
+        {
+            Console.Write("What is your Guess? ");
+            string Number = Console.ReadLine();
+            number = int.Parse(Number);
+
+
+
+            if (magic > number)
+            {
+                Console.WriteLine("Higher");
+                
+            }
+            else if (magic < number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+    //    Console.WriteLine($" {magic} and {number}");
     }
 }
