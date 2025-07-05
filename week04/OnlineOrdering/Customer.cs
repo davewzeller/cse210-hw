060685cae161cd5555de5e303d71a7b5a8eb5402
Dave Zeller <59622979+davewzeller@users.noreply.github.com>
2025-07-05 16:07:57 -0700

    complied with customer

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,24 @@
+public class Customer
+{
+    private string _name;
+    private Address _address;
+
+    public Customer(String nameInput, Address addressInput)
+    {
+
+        _name = nameInput;
+        _address = addressInput;
+    }
+
+    public bool LivesInUsa()
+    {
+        return _address.IsInUSA();
+    }
+
+    public string GetName() => _name;
+    public string GetAddressLabel() => _address.GetFullAddress();
+
+
+
+
+}
