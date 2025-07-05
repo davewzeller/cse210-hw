808b82466bfcaabfd05e5018033ec12a60e927ab
Dave Zeller <59622979+davewzeller@users.noreply.github.com>
2025-07-05 16:40:50 -0700

    Sent info to customer

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -6,6 +6,6 @@ class Program
     static void Main(string[] args)
     {
         Address address1 = new Address("742 Evergreen Terrace", "SpringField", "Oregon 97403", "USA");
-        
+        Customer customer = new Customer("Homer Simpson", address1);
     }
 }
