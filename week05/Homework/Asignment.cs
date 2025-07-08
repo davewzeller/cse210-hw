3ca3f29b3b089cb528fcb609dc94b556d6660746
Dave Zeller <59622979+davewzeller@users.noreply.github.com>
2025-07-08 10:52:08 -0700

    Asignment Class

---
+++ week05/Homework/Asignment.cs
@@ -0,0 +1,12 @@
+using System.Reflection.Emit;
+using System.Text;
+
+public class Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+    public void GetSummary()
+    {
+        Console.WriteLine("Hi Dave");
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment();
+        assignment.GetSummary();
     }
 }
