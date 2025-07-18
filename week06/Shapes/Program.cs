85419272a9914add8d354d5edca0eac9571fdc12
Dave Zeller <59622979+davewzeller@users.noreply.github.com>
2025-07-17 20:30:16 -0700

    List Added.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shapes> shapes = new List<Shapes>();
+        //Console.WriteLine("Hello World! This is the Shapes Project.");
+
+
+        
     }
 }
