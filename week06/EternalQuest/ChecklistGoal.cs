64da28741caf9b53259a5e7440fd989b61c6541c
Dave Zeller <59622979+davewzeller@users.noreply.github.com>
2025-07-19 14:46:04 -0700

    checklistgoal

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,30 @@
+public class ChecklistGoal : Goal
+{
+
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus)
+        : base(name, description, points)
+
+    {
+        _target = target;
+        _bonus = bonus;
+        _amountCompleted = 0;
+    }
+
+    public override int RecordEvent()
+    {
+        _amountCompleted++;
+        return _amountCompleted >= _target ? _points + _bonus : _points;
+    }
+
+    public override bool IsComplete() => _amountCompleted >= _target;
+
+    public override string GetStringRepresentation() =>
+        $"ChecklistGoal: {_shortName} | {_description} | {_points} | {_target} | {_bonus} | {_amountCompleted}";
+
+  
+}
