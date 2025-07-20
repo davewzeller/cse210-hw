e9083b3f5f8028be98dc7399ba1aa6608ab6c355
Dave Zeller <59622979+davewzeller@users.noreply.github.com>
2025-07-20 07:51:27 -0700

    PromptGenerator Repaired

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,48 +1,34 @@
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.IO;
+
 public class PromptGenerator
 
 {
 
 
-    public static string _prompt()
-    {
-        
-        List<string> _prompt = new List<string>();
+    private static List<string> _prompt = new List<string>
+        {
+              "Today I found joy in: ",
+              "Things I did for myself: ",
+              "Things I did for Others: ",
+              "Today I am grateful for: ",
+              "Inspiring words for today: ",
+              "Things to let go of today: "
 
-        _prompt.Add("Today I found joy in: ");
-        _prompt.Add("Things I did for myself: ");
-        _prompt.Add("Things I did for Others: ");
-        _prompt.Add("Today I am grateful for: ");
-        _prompt.Add("Inspiring words for today: ");
-        _prompt.Add("Things to let go of today: ");
+        };
 
 
-        Random randomGenerator = new Random();
-        int magic = randomGenerator.Next(0, _prompt.Count);
-        // thank goodness for my brother in law. 
+    private static Random _rand = new Random();
 
-        //Console.WriteLine($"{magic}");
-        Console.Write($"{_prompt[magic]}");
-        return _prompt[magic];
+    public static string GetRandomPrompt()
+        {
+        int _index = _rand.Next(_prompt.Count);
+        return _prompt[_index];
+
+        }
         
     }
 
 
 
 
-
-/*    public int GetRandomPrompt()
-    {
-        Random randomGenerator = new Random();
-        int magic = randomGenerator.Next(1, 5);
-
-
-
-        return magic;
-    }
-*/    
+    
 }
