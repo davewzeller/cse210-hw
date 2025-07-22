02b56cf973c136c500475de0ab42f078c9d5e07f
Dave Zeller <59622979+davewzeller@users.noreply.github.com>
2025-07-21 17:11:50 -0700

    Swimming is done.

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -8,19 +8,21 @@ class Program
         List<Activity> activities = new List<Activity>
         {
             new Running("03 Nov 2025", 30, 28),
-            new Cycling("21 July 2025", 30, 183.9) 
-   //         new Swimming("24 Aug 2025", 30, 20)
+            new Cycling("21 July 2025", 30, 183.9),
+            new Swimming("24 Aug 2025", 30, 20)
 
 
         };
 
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine("\nHello World! This is the ExerciseTracking Project.\n");
 
         foreach (Activity a in activities)
         {
 
             Console.WriteLine(a.GetSummary());
 
-        }    
+
+        }
+        Console.WriteLine();
     }
 }
